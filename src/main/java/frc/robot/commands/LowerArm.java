aea3bf5869992f47a363be556eab4bae5e0e6c68
mps-green <mps-green@team1675.org>
2019-01-19 15:03:03 -0600

    added most code for the arm/hatch manipulator

--- src/main/java/frc/robot/commands/LowerArm.java
+++ src/main/java/frc/robot/commands/LowerArm.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -10,13 +10,12 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-/**
- * An example command.  You can replace me with your own command.
- */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+
+public class LowerArm extends Command {
+  public LowerArm() {
     // Use requires() here to declare subsystem dependencies
-    requires(Robot.driveBase);
+    // eg. requires(chassis);
+    requires(Robot.grabber);
   }
 
   // Called just before this Command runs the first time
@@ -27,12 +26,13 @@ public class ExampleCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.grabber.lowerArm();
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return true;
   }
 
   // Called once after isFinished returns true
@@ -44,5 +44,6 @@ public class ExampleCommand extends Command {
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    end();
   }
 }
