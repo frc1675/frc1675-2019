497fa96faaca5896c906c30e92239781e0ef0eed
Lanning <lanninjd@milwaukee.k12.wi.us>
2019-02-09 14:39:06 -0600

    developed the turnforangle command further

---
+++ src/main/java/frc/robot/commands/TurnForAngle.java
@@ -0,0 +1,69 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.command.PIDCommand;
+import frc.robot.subsystems.DriveBase;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+public class TurnForAngle extends PIDCommand {
+  int count = 0;
+  double angleturned;
+  double timeout;
+  double initialDegrees;
+  double setpoint;
+  public TurnForAngle(double angleturned, double timeout) {
+    super (RobotMap.Gyro_P,RobotMap.Gyro_I,RobotMap.Gyro_D);
+    this.angleturned = angleturned;
+    this.timeout = timeout;
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    this.getPIDController().reset();
+    this.getPIDController().setOutputRange(-.5, .5);
+  //  initialDegrees = Robot.driveBase.getAngle();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+  @Override
+  protected double returnPIDInput(){
+  }
+  @Override
+  protected void usePIDOutput(double output){
+    Robot.driveBase.setRightMotors(-output);
+    Robot.driveBase.setLeftMotors(output);
+  }
+  
+}
